fa077eed9f8346b5ba2260e0c56e90378ce61379
NguyenCoDuoc <coduoc2502@gmail.com>
2017-06-21 23:05:53 +0700

    Finish user signup-01

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".welcome_to_the_sample_app"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by params[:id]
+
+    unless @user
+      flash.now[:danger] = t ".note"
+      render file: "public/404.html"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require :user.permit :name, :email, :password,
+      :password_confirmation
   end
 end
